b9586691537f0812ce3008cf51e8bdce70a9e26b
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2025-10-01 09:39:27 -0400

    Add files via upload

---
+++ Subprogram Exercises/Subprrogram Exercises/Form1.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button6 = New Button()
+        Button7 = New Button()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button8 = New Button()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Label5 = New Label()
+        Button9 = New Button()
+        TextBox7 = New TextBox()
+        Label6 = New Label()
+        Button10 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(184, 13)
+        ListBox1.Margin = New Padding(4, 3, 4, 3)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(213, 274)
+        ListBox1.TabIndex = 8
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.White
+        Button1.Location = New Point(15, 144)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(144, 23)
+        Button1.TabIndex = 9
+        Button1.Text = "Countdown From 10"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.White
+        Button2.Location = New Point(15, 174)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(144, 23)
+        Button2.TabIndex = 10
+        Button2.Text = "Countdown From 100"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.White
+        Button3.Location = New Point(15, 203)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(144, 23)
+        Button3.TabIndex = 11
+        Button3.Text = "Countdown From 500"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.White
+        Button4.Location = New Point(12, 31)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(144, 23)
+        Button4.TabIndex = 12
+        Button4.Text = "Sum of 200 and 300"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.White
+        Button5.Location = New Point(12, 60)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(144, 23)
+        Button5.TabIndex = 13
+        Button5.Text = "Sum of 2 Inputs"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(14, 89)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(62, 23)
+        TextBox1.TabIndex = 14
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(82, 89)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(62, 23)
+        TextBox2.TabIndex = 15
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label1.ForeColor = Color.White
+        Label1.Location = New Point(15, 13)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(32, 15)
+        Label1.TabIndex = 16
+        Label1.Text = "Sum"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label2.ForeColor = Color.White
+        Label2.Location = New Point(18, 126)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(71, 15)
+        Label2.TabIndex = 17
+        Label2.Text = "Countdown"
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.White
+        Button6.Location = New Point(15, 259)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(144, 23)
+        Button6.TabIndex = 18
+        Button6.Text = "Double It"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.White
+        Button7.Location = New Point(15, 319)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(144, 23)
+        Button7.TabIndex = 19
+        Button7.Text = "Midpoint"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label3.ForeColor = Color.White
+        Label3.Location = New Point(18, 241)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(59, 15)
+        Label3.TabIndex = 20
+        Label3.Text = "Double It"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label4.ForeColor = Color.White
+        Label4.Location = New Point(18, 301)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(57, 15)
+        Label4.TabIndex = 21
+        Label4.Text = "Midpoint"
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.White
+        Button8.Location = New Point(15, 348)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(144, 23)
+        Button8.TabIndex = 22
+        Button8.Text = "Midpoint with Inputs"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(52, 377)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(30, 23)
+        TextBox3.TabIndex = 24
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(16, 377)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(30, 23)
+        TextBox4.TabIndex = 23
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(124, 377)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(30, 23)
+        TextBox5.TabIndex = 26
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(88, 377)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(30, 23)
+        TextBox6.TabIndex = 25
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label5.ForeColor = Color.White
+        Label5.Location = New Point(184, 302)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(38, 15)
+        Label5.TabIndex = 27
+        Label5.Text = "Plural"
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.White
+        Button9.Location = New Point(184, 319)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(144, 23)
+        Button9.TabIndex = 28
+        Button9.Text = "Is it plural?"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(334, 319)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(86, 23)
+        TextBox7.TabIndex = 29
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label6.ForeColor = Color.White
+        Label6.Location = New Point(184, 356)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(102, 15)
+        Label6.TabIndex = 30
+        Label6.Text = "Collatz Sequence"
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.White
+        Button10.Location = New Point(184, 374)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(144, 23)
+        Button10.TabIndex = 31
+        Button10.Text = "Collatz"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.MediumSlateBlue
+        ClientSize = New Size(432, 422)
+        Controls.Add(Button10)
+        Controls.Add(Label6)
+        Controls.Add(TextBox7)
+        Controls.Add(Button9)
+        Controls.Add(Label5)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox4)
+        Controls.Add(Button8)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Subprogram Exercises"
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button8 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button9 As Button
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button10 As Button
+End Class
